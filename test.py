7b98982fcfdaf9dcc3160eac9be6fe3cc6951d1a
MZ <lmzohio@gmail.com>
2018-01-25 05:02:19 -0300

    test function call

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print('hello')
+#print('hello')
+#print(2 ** 10 * (2 ** 64))
+#print('兔纸')#
+#print(250 + 2500)
+
+
+def function_1(a, b):
+    """
+
+    :rtype: object
+    """
+    return (a + b)
+
+
+print(function_1(12, 13))
