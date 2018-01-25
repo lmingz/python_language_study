37e3b65cf1df4f5a464d135c2ce995bef756073c
Mingzhi Leng <lmzohio@gmail.com>
2018-01-25 15:59:26 -0800

    添加九九表

---
+++ test/test_function_call.py
@@ -0,0 +1,8 @@
+def out_put_jiujiubiao(a,b):
+    i=1
+    j=1
+    for i in range (1,a+1):
+        for j in range (1,b+1):
+            if i>=j:
+                print (j, '*', i ,'=' ,i*j)
+out_put_jiujiubiao(9,9)
